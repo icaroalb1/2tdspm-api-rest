dd013c2583b32cb6f46d174fb76946e8c1eafee9
Luiz Gustavo S. Real <lgsreal2@gmail.com>
2025-02-25 12:14:12 -0300

    aula 25-02-2025

--- src/main/java/br/com/fiap/api_rest/controller/ClienteController.java
+++ src/main/java/br/com/fiap/api_rest/controller/ClienteController.java
@@ -7,11 +7,14 @@ import br.com.fiap.api_rest.repository.ClienteRepository;
 import br.com.fiap.api_rest.service.ClienteService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,9 +36,9 @@ public class ClienteController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ClienteResponse>> readClientes() {
-        List<Cliente> clientes = clienteRepository.findAll();
-        return new ResponseEntity<>(clienteService.clientesToResponse(clientes), HttpStatus.OK);
+    public ResponseEntity<Page<ClienteResponse>> readClientes(@RequestParam(required = true) int page) {
+        Pageable pageable = PageRequest.of(page, 2, Sort.by("categoria").ascending().and(Sort.by("nome").ascending()));
+        return new ResponseEntity<>(clienteService.findAll(pageable), HttpStatus.OK);
     }
 
     // PathVariable = parâmetro diretamente na URL, ex: /clientes/1
